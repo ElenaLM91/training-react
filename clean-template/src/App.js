169d3478cca6f1def4c0e0672d84921a73dd5a52
Helen <elena-lopezmoreno@hotmail.com>
2022-11-22 19:55:13 +0100

    1

---
+++ clean-template/src/App.js
@@ -0,0 +1,19 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita alias
+        asperiores rerum harum recusandae saepe, repellat id quos tempore quo
+        quam aperiam eum nisi iste suscipit deleniti maxime voluptates
+        molestiae, velit amet laudantium ducimus ut. Ullam quaerat distinctio
+        placeat laborum eligendi ab, ratione quae! Dolore laudantium animi et
+        laborum unde.
+      </p>
+    </div>
+  );
+}
+
+export default App;
