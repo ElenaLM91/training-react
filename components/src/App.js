9bfae1e971a25cc9d361e742645a5806adda3d03
Helen <elena-lopezmoreno@hotmail.com>
2022-11-23 18:09:42 +0100

    ejemplo minimo import

--- components/src/App.js
+++ components/src/App.js
@@ -1,17 +1,13 @@
-// import './App.css';
+import Title from "./components/Title";
+import Paragraph from "./components/Paragraph";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita alias
-        asperiores rerum harum recusandae saepe, repellat id quos tempore quo
-        quam aperiam eum nisi iste suscipit deleniti maxime voluptates
-        molestiae, velit amet laudantium ducimus ut. Ullam quaerat distinctio
-        placeat laborum eligendi ab, ratione quae! Dolore laudantium animi et
-        laborum unde.
-      </p>
+    <div className="main">
+      <Title></Title>
+      <Paragraph></Paragraph>
+      <Paragraph></Paragraph>
     </div>
   );
 }
---
+++ components/src/components/Paragraph.js
@@ -0,0 +1,14 @@
+import "./Paragraph.css";
+
+function Paragraph() {
+  return (
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut excepturi
+      magnam, nisi quae, dolorum suscipit, porro maiores quisquam saepe ducimus
+      distinctio provident ab nulla voluptate fuga commodi obcaecati modi
+      incidunt!
+    </p>
+  );
+}
+
+export default Paragraph;
