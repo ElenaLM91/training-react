02b6f5ae581ef4e0122b4425802738f7e48bcc69
Helen <elena-lopezmoreno@hotmail.com>
2022-12-01 17:27:31 +0100

    Ejercicios movies

--- clean-template/src/App.js
+++ clean-template/src/App.js
@@ -1,92 +1,139 @@
-// // import './App.css';
-// import { useState } from "react";
-// import Person from "./Components/Person"
+// import './App.css';
 
 // function App() {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div>
-//       <Person/>
-//      {/* <p>Has clicado {count} veces</p> /}
-//       {/ <button onClick={()=> setCount(count +1)}>Pulsame</button> */}
-//     </div>
-//   );
-// }
-
-// export default App;
-// // import './Person.css';
-// import { useState } from "react";
-
-// function Person() {
-//     const [person, setPersonInfo] = useState({
-//         name:"John",
-//         age:27,
-//         enemys:["Skynet","T-800","T-1000","T-x"],
-//         father:"unknown"
-//     });
-//     const updateFather = ()=>{
-//        setPersonInfo(previousState=>{
-//         return{
-//             ...previousState,father:"Kyle Reese"
-//         }
-//        })
+//   const people = [
+//     {
+//       name: 'James',
+//       age: 31,
+//     },
+//     {
+//       name: 'John',
+//       age: 45,
+//     },
+//     {
+//       name: 'Paul',
+//       age: 65,
+//     },
+//     {
+//       name: 'Ringo',
+//       age: 49,
+//     },
+//     {
+//       name: 'George',
+//       age: 34,
 //     }
-//   return (
+//   ];
+//   return  (
 //     <div>
-//         <h1>Character info</h1>
-//         <h2>Name: {person.name}</h2>
-//         <h2>Age: {person.age}</h2>
-//         <h2>Enemys: {person.enemys}</h2>
-//         <h2>Father: {person.father}</h2>
-//         <button type="button" onClick={updateFather}>Update</button>
-
-//     </div>
-//   );
-// }
-
-// export default Person;
-
-// ----------------------------------------------------------------------
-// //import './App.css';
-// import { useState } from "react";
-
-// import Counter from "./components/Counter";
-
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   const handlePlusOne = () => {
-//     setCount(count + 1);
-//   };
-//   const handleTimesFive = () => {
-//     setCount(count * 5);
-//   };
-//   const handleMinusOne = () => {
-//     setCount(count - 1);
-//   };
-//   const handleDivideByHundred = () => {
-//     if (count !== 0) {
-//       setCount(count / 100);
-//     } else {
-//       console.log("Division of 0 not allowed");
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <Counter count={count} />
-//       <button onClick={handlePlusOne}>Pulsame para sumar</button>
-//       <button onClick={handleTimesFive}>Pulsame para multiplicar por 5</button>
-//       <button onClick={handleMinusOne}>Pulsame para restar</button>
-//       <button onClick={handleDivideByHundred}>
-//         Pulsame para dividir por 100
-//       </button>
+//       {people.filter(person => person.age < 60).map(filteredPerson => (
+//         <li>
+//         <strong>Name:</strong> {filteredPerson.name} <strong>Age:</strong> {filteredPerson.age}
+//         </li>
+//       ))}
 //     </div>
 //   );
 // }
 
 // export default App;
 
+// // Solución formateada en una tabla
+// //   <table>
+// //       <thead>
+// //         <th>
+// //           <td>Name</td>
+// //           <td>Age</td>
+// //         </th>
+// //       </thead>
+// //       <tbody>
+// //         {filteredList.map((member) => (
+// //           <tr>
+// //             <td>
+// //               <strong>Name: </strong> {member.first_name}
+// //             </td>
+// //             <strong>Age: {member.age}</strong>
+// //           </tr>
+// //         ))}
+// //       </tbody>
+// //     </table>
 
+import "./App.css";
+import listMovies from "./components/Movies/Movies";
 
+function App() {
+  const memberList = [
+    {
+      id: 1,
+      first_name: "Byram",
+      age: 56,
+    },
+    {
+      id: 2,
+      first_name: "Clementius",
+      age: 67,
+    },
+    {
+      id: 3,
+      first_name: "Leonid",
+      age: 33,
+    },
+    {
+      id: 4,
+      first_name: "Ashli",
+      age: 86,
+    },
+    {
+      id: 5,
+      first_name: "Dale",
+      age: 15,
+    },
+    {
+      id: 6,
+      first_name: "Tremain",
+      age: 83,
+    },
+    {
+      id: 7,
+      first_name: "Magdalene",
+      age: 40,
+    },
+    {
+      id: 8,
+      first_name: "Robb",
+      age: 87,
+    },
+    {
+      id: 9,
+      first_name: "Frankie",
+      age: 11,
+    },
+    {
+      id: 10,
+      first_name: "Hammad",
+      age: 37,
+    },
+  ];
+  const filteredList = memberList.filter((member) => {
+    return member.age < 50;
+  });
+
+  <table>
+    <thead>
+      <th>
+        <td>Name</td>
+        <td>Age</td>
+      </th>
+    </thead>
+    <tbody>
+      {filteredList.map((member) => (
+        <tr>
+          <td>
+            <strong>Name: </strong> {member.first_name}
+          </td>
+          <strong>Age: {member.age}</strong>
+        </tr>
+      ))}
+    </tbody>
+  </table>;
+}
+
+export default App;
---
+++ clean-template/src/components/Movies/Movies.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./Movies.css";
+import movies from "../db/movies.json";
+import MoviesWrappers from "./MoviesWrapper";
+import MoviesMain from "./MoviesMain";
+import Card from "./Card";
+import MoviesActions from "./MoviesActions";
+
+function Movies() {
+  const [moviesList, setMoviesList] = useState(movies);
+  const [fadeIn, setFadeIn] = useState(true);
+  const changeVisibility = (toShow) => {
+    setFadeIn(false);
+    setTimeout(() => {
+      setFadeIn(true);
+      setMoviesList(toShow);
+    }, 500);
+  };
+
+  const filterComedy = () => {
+    let comedyMovies = movies.filter((movie) => {
+      return movie.genre.includes("Comedy");
+    });
+    changeVisibility(comedyMovies);
+  };
+  const filterDrama = () => {
+    let dramaMovies = movies.filter((movie) => {
+      return movie.genre.includes("Drama");
+    });
+    changeVisibility(dramaMovies);
+  };
+  const noFilter = () => {
+    changeVisibility(movies);
+  };
+
+  return (
+    <MoviesMain>
+      <MoviesActions
+        onFilterComedy={filterComedy}
+        onFilterDrama={filterDrama}
+        onNoFilter={noFilter}
+      />
+      <MoviesWrappers>
+        {moviesList.map((movie) => (
+          <Card key={movie.id} movie={movie} fadeIn={fadeIn} />
+        ))}
+      </MoviesWrappers>
+    </MoviesMain>
+  );
+}
+
+export default Movies;
